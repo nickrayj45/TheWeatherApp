fc66589e036f440f0d5cea0514bbb0d4aff4d27e
Nicholas Jaquez <nrjaquez@gmail.com>
2020-02-08 23:48:46 -0800

    updated with API Keys working

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // Going to need the weather API Key
-var queryURL = "58cf44a546a393fc5b1a3c754ae6b266"
+var APIKey = "fea808ff5dc02267acae44912e35ab54";
 
+$("#btn").on("click", function() {
+  cityName = $("#inputField").val();
+
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    APIKey;
+
+  // var cityName = $("#inputField").on("click").val();
+  var cityName = $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    $("#cityInfo").text(response);
+    var nameOfCity = $("<h1>").text(response.name);
+    $(".col-md-8").append(nameOfCity);
+    var currentTemp = $("<h2>").text(response.main[0]);
+    $(".col-md-2").append(currentTemp);
+    var currentHumidity = $("<h2>").text(response.main[5]);
+    $(".col-md-2").append(currentHumidity);
+  });
+});
+
+// Start to append the weather content to the main card
